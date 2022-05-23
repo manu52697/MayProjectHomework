5209b4368a7002444fd778200be6045d18f52e69
manu52697 <32219329+manu52697@users.noreply.github.com>
2022-05-23 20:08:35 +0200

    Fix BaseEntity to support tracking of creator, updator and deletor Users.

--- UniversityAPIBackend/Models/DataModels/BaseEntity.cs
+++ UniversityAPIBackend/Models/DataModels/BaseEntity.cs
@@ -9,13 +9,13 @@ namespace UniversityAPIBackend.Models.DataModels
         [Key]
         public int Id { get; set; }
 
-        public string CreatedBy { get; set; } = string.Empty;
+        public User? CreatedBy { get; set; } 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
-        public string UpdatedBy { get; set; } = string.Empty;
-        public DateTime UpdatedAt { get; set; }
-        public string DeletedBy { get; set; } = string.Empty;
-        public DateTime DeletedAt { get; set; }
+        public User? UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public User? DeletedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
         public bool IsDeleted { get; set; } = false;
     }
 }
