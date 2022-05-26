5e1e2dd3a4a11e3f61e1b51b0bc2142d293577c8
manu52697 <32219329+manu52697@users.noreply.github.com>
2022-05-26 21:27:13 +0200

    Add BaseQueryService as base class for future query services

---
+++ UniversityAPIBackend/Services/BaseQueryService.cs
@@ -0,0 +1,14 @@
+﻿using UniversityAPIBackend.DataAccess;
+
+namespace UniversityAPIBackend.Services
+{
+    public class BaseQueryService
+    {
+        protected readonly UniversityDBContext _dbContext;
+
+        public BaseQueryService(UniversityDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
