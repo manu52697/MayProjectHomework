42d9f479a031e34835c82d55edb782491c3fe09d
manu52697 <32219329+manu52697@users.noreply.github.com>
2022-05-26 21:27:35 +0200

    Implement IStudentService methods

--- UniversityAPIBackend/Services/StudentsService.cs
+++ UniversityAPIBackend/Services/StudentsService.cs
@@ -1,18 +1,38 @@
-﻿using UniversityAPIBackend.Models.DataModels;
+﻿using UniversityAPIBackend.DataAccess;
+using UniversityAPIBackend.Models.DataModels;
 
 namespace UniversityAPIBackend.Services
 {
-    public class StudentsService : IStudentsService
+    public class StudentsService : BaseQueryService ,IStudentsService
     {
-        // TODO: resolve methods
+        public StudentsService(UniversityDBContext dbContext) : base(dbContext)
+        {
+        }
+
+
         public IEnumerable<Student> GetStudentsWithCourses()
         {
-            throw new NotImplementedException();
+            return GetStudentsByEnrollment(true);
         }
 
         public IEnumerable<Student> GetStudentsWithNoCourses()
         {
-            throw new NotImplementedException();
+            return GetStudentsByEnrollment(false);
+        }
+
+        // Returns students with or without any courses, conditional on `searchEnrolled`
+        private IEnumerable<Student> GetStudentsByEnrollment(bool searchEnrolled)
+        {
+
+            if(_dbContext.Students != null)
+            {
+                if (searchEnrolled)
+                {
+                    return _dbContext.Students.Where(s => s.Courses.Any());
+                }
+                return _dbContext.Students.Where(s => !s.Courses.Any());
+            }
+            return new List<Student>();
         }
     }
 }
